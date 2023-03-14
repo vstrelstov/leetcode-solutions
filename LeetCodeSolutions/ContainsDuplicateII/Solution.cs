b9d741d767165bf41c7d7b95b3a75bf332f5e114
Vasiliy Streltsov <streltcov.vasiliy.bpo1401@yandex.ru>
2023-03-14 22:27:57 +0300

    LeetCode-219: More nifty Solution.cs

--- LeetCodeSolutions/ContainsDuplicateII/Solution.cs
+++ LeetCodeSolutions/ContainsDuplicateII/Solution.cs
@@ -45,4 +45,20 @@ internal class Solution
 
         return false;
     }
+
+    public bool ContainsNearbyDuplicateMap(int[] nums, int k)
+    {
+        var map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (map.ContainsKey(nums[i]) && (i - map[nums[i]]) <= k)
+            {
+                return true;
+            }
+
+            map[nums[i]] = i;
+        }
+
+        return false;
+    }
 }
